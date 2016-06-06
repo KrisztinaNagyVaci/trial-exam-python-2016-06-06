3dd03a8f01cfbf401baf05c0921882d6340a8fcb
Kriszti <krisztina.vaci@gmail.com>
2016-06-06 09:53:54 +0200

    1, 2

--- 1.py
+++ 1.py
@@ -1,3 +1,15 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all it's element value doubled.
 # It should raise an error if the parameter is not a list
+
+exampleList = [1, 2, 3, 4]
+examplestring = 'hollywood'
+
+
+def listElementDoubler(listInput):
+    outputList = []
+    for i in listInput:
+        outputList.append(i * 2)
+    return outputList
+
+print(listElementDoubler(exampleList))
--- 2.py
+++ 2.py
@@ -1,3 +1,20 @@
 # Create a function that takes a filename as string parameter,
 # and counts the occurances of the letter "a", and returns it as a number.
 # It should not break if the file not exists just return 0
+
+
+
+def returnAasNumber(filename):
+    try:
+        f = open(filename)
+        text = f.read()
+        f.close()
+        numberOfA = 0
+        for i in text:
+            if i == 'a':
+                numberOfA = numberOfA + 1
+        return numberOfA
+    except FileNotFoundError:
+        return 0
+
+print(returnAasNumber('holiday.txt'))
