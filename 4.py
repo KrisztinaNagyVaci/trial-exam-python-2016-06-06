88843c938e9a1471f2e7ea2a0c4ba37f2f8e1def
Kriszti <krisztina.vaci@gmail.com>
2016-06-06 10:49:18 +0200

    Task 4, 5

--- 4.py
+++ 4.py
@@ -1,7 +1,23 @@
 # Write unittests for this function:
+import unittest
+
 
 def greeter(name):
   if len(name):
     return 'Hello, ' + name + '!'
   else:
     return 'Hello, Mr Nobody!'
+
+class MyTest(unittest.TestCase):
+    def test_string_input_was_given(self):
+        self.assertEqual(greeter('Malinda'), 'Hello, Malinda!')
+
+    def test_no_input_was_given(self):
+        self.assertEqual(greeter(''), 'Hello, Mr Nobody!')
+
+    # def test_integer_input_was_given(self):
+        # self.assertNotEqual(greeter(3), 'Hello, 3!')
+
+
+if __name__ == '__main__':
+    unittest.main()
